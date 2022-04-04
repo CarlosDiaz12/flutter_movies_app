8c7bd07e897147815591fc3380a134e12ce11368
Carlos Diaz <carlos.cedp@gmail.com>
2022-04-04 14:47:21 -0400

    💄 ✨ favorite movie list working

--- lib/ui/pages/favorite_movies/favorite_movies_page.dart
+++ lib/ui/pages/favorite_movies/favorite_movies_page.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_app/core/constants/remote_constants.dart';
 import 'package:flutter_movies_app/core/error/exceptions.dart';
+import 'package:flutter_movies_app/data/local/dto/movie_local_dto.dart';
+import 'package:flutter_movies_app/data/local/movie_local_dao.dart';
 import 'package:flutter_movies_app/ui/pages/favorite_movies/favorite_movies_viewmodel.dart';
+import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
 
 class FavoriteMoviesPage extends StatelessWidget {
@@ -11,10 +16,14 @@ class FavoriteMoviesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<FavoriteMoviesViewModel>.reactive(
       builder: (context, viewModel, _) => Scaffold(
-        appBar: AppBar(title: Text('Favorite Movies')),
+        appBar: AppBar(centerTitle: true, title: Text('Favorite Movies')),
         body: viewModel.hasError ? _ErrorWidget() : _MainBody(),
       ),
-      viewModelBuilder: () => FavoriteMoviesViewModel(),
+      onModelReady: (viewModel) async {
+        await viewModel.loadData();
+      },
+      viewModelBuilder: () => FavoriteMoviesViewModel(
+          movieLocalDao: Provider.of<MovieLocalDao>(context)),
     );
   }
 }
@@ -24,7 +33,7 @@ class _MainBody extends ViewModelWidget<FavoriteMoviesViewModel> {
 
   @override
   Widget build(BuildContext context, viewModel) {
-    var imageHeight = MediaQuery.of(context).size.height * 0.80;
+    var imageHeight = MediaQuery.of(context).size.height * 0.75;
     var imageWidth = MediaQuery.of(context).size.width * 0.90;
     return viewModel.isBusy
         ? Center(
@@ -33,31 +42,110 @@ class _MainBody extends ViewModelWidget<FavoriteMoviesViewModel> {
         : Container(
             margin: EdgeInsets.symmetric(vertical: 8),
             constraints: BoxConstraints(maxHeight: imageHeight),
-            child: ListView.separated(
-              separatorBuilder: (ctx, int) => SizedBox(width: 8),
-              itemCount: 2,
-              cacheExtent: 6,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  elevation: 4,
-                  margin: EdgeInsets.all(8),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      child: Image.network(
-                        '${RemoteConstants.IMAGE_API_URL}/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg',
-                        cacheWidth: imageWidth.toInt(),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+            child: viewModel.favoriteMovies!.length == 0
+                ? Center(
+                    child: Text(
+                    'No favorite movies added',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ))
+                : ListView.builder(
+                    itemCount: viewModel.favoriteMovies?.length ?? 0,
+                    cacheExtent: 6,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      var item = viewModel.favoriteMovies![index];
+                      return _FavoriteMovieItem(
+                          movie: item, imageWidth: imageWidth);
+                    },
                   ),
-                );
-              },
-            ),
           );
+  }
+}
+
+class _FavoriteMovieItem extends StatelessWidget {
+  final MovieLocalDto movie;
+  const _FavoriteMovieItem({
+    required this.movie,
+    Key? key,
+    required this.imageWidth,
+  }) : super(key: key);
+
+  final double imageWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          elevation: 4,
+          margin: EdgeInsets.all(16),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              child: Image.network(
+                '${RemoteConstants.IMAGE_API_URL}${movie.poster_path}',
+                cacheWidth: imageWidth.toInt(),
+                fit: BoxFit.cover,
+                color: Colors.black.withOpacity(0.3),
+                colorBlendMode: BlendMode.darken,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 40,
+          left: 30,
+          right: 30,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _VoteAverageWidget(
+                vote_average: movie.vote_average,
+              ),
+              Container(
+                width: 250,
+                child: Text(
+                  '${movie.title?.toUpperCase()}',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _VoteAverageWidget extends StatelessWidget {
+  final double? vote_average;
+  const _VoteAverageWidget({
+    this.vote_average,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 25,
+      backgroundColor: Colors.white,
+      child: Text(
+        '$vote_average',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
   }
 }
 
--- lib/ui/pages/favorite_movies/favorite_movies_viewmodel.dart
+++ lib/ui/pages/favorite_movies/favorite_movies_viewmodel.dart
@@ -1,3 +1,19 @@
+import 'package:flutter_movies_app/data/local/dto/movie_local_dto.dart';
+import 'package:flutter_movies_app/data/local/movie_local_dao.dart';
 import 'package:stacked/stacked.dart';
 
-class FavoriteMoviesViewModel extends BaseViewModel {}
+class FavoriteMoviesViewModel extends BaseViewModel {
+  final MovieLocalDao movieLocalDao;
+  List<MovieLocalDto>? _favoriteMovies;
+  List<MovieLocalDto>? get favoriteMovies => _favoriteMovies;
+  FavoriteMoviesViewModel({
+    required this.movieLocalDao,
+  });
+
+  Future<void> loadData() async {
+    setBusy(true);
+    var res = movieLocalDao.getFavoriteMovies();
+    _favoriteMovies = res;
+    setBusy(false);
+  }
+}
